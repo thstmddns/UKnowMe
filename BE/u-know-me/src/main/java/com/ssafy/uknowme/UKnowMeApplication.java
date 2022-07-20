2eddb58d3f4860a83f8cbdf1b8ebf9973faeb7b9
이명범 <mungmnb777@gmail.com>
2022-07-20 14:13:27 +0900

    initial commit

---
+++ BE/u-know-me/src/main/java/com/ssafy/uknowme/UKnowMeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.uknowme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UKnowMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UKnowMeApplication.class, args);
+	}
+
+}
