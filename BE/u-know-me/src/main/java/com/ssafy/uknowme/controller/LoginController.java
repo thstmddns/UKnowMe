6f0b710784df7b64b4ff43a0b1c08d7656a67c65
손승운 <thstmddns@naver.com>
2022-07-27 08:42:34 +0900

    SpringConfig구현[S07P12D104-50] feat: 회원정보 생성, 커트롤러, dto생성 2

--- BE/u-know-me/src/main/java/com/ssafy/uknowme/controller/LoginController.java
+++ BE/u-know-me/src/main/java/com/ssafy/uknowme/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.ssafy.uknowme.controller;
 
 import com.ssafy.uknowme.service.LoginService;
+import com.ssafy.uknowme.web.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,8 +12,8 @@ public class LoginController {
     private final LoginService loginService;
 
     @GetMapping("/login")
-    public String loginId(@ModelAttribute User user) {
-        if (loginService.login(user)){
+    public String loginId(@ModelAttribute Member member) {
+        if (loginService.login(member)){
             return "redirect:/";
         }
         return "login";
