8cff7de7af026d810e5f1f4d02dcfd228552b4bd
이동민 <jewellove1023@gmail.com>
2022-07-29 11:01:39 +0900

    [S07P12D104-59] feat: 로그인 모달 제작 #2
    로그인 디자인 변경 및 소셜 로그인 버튼 위치 변경

--- FE/u-know-me/src/components/land/modal/SignIn.vue
+++ FE/u-know-me/src/components/land/modal/SignIn.vue
@@ -4,29 +4,44 @@
     <div style="font-size:16px;">Welcome Back</div>
   </div>
   <form id="signInForm" action="POST" @submit.prevent="account.login(credentials)">
-    <div class="signin-input">
-      <div><label for="signInId">아이디</label></div>
-      <div><input type="text" name="signInId" id="signInId" v-model="credentials.id"/></div>
+    <div class="signin-input flex">
+      <div class="icon-cell">
+        <div class="icon-signin">
+          <i class="fa-solid fa-user"></i>
+        </div>
+      </div>
+      <div>
+        <input type="text" name="signInId" id="signInId" placeholder="아이디" v-model="credentials.id"/>
+      </div>
     </div>
-    <div class="signin-input">
-      <div><label for="signInPassword">비밀번호</label></div>
-      <div><input type="text" name="signInPassword" id="signInPassword" v-model="credentials.password"/></div>
+    <div class="signin-input flex">
+      <div>
+        <div class="icon-signin">
+          <i class="fa-solid fa-unlock-keyhole"></i>
+        </div>
+      </div>
+      <div>
+        <input type="password" name="signInPassword" id="signInPassword" placeholder="비밀번호" v-model="credentials.password"/>
+      </div>
     </div>
     <div class="find-id-password">
       <span>아이디/</span><span>비밀번호 찾기</span>
     </div>
-    <div class="flex justify-space-between align-center">
+    <button class="login-btn">로그인</button>
+  </form>
+    <p class="login-signup">
+      <span @click="land.btnCh=2">계정이 필요하신가요?</span>
+    </p>
+  <div class="hr-sect">
+    SNS 계정으로 로그인하기
+  </div>
+  <div class=" flex justify-center align-center">
       <div class="flex">
         <img class="sns-login" src="@/assets/land/naver_login_icon.png" alt="naver_login_icon">
         <img class="sns-login" src="@/assets/land/google_login_icon.png" alt="google_login_icon">
         <img class="sns-login" src="@/assets/land/kakao_login_icon.png" alt="kakao_login_icon">
       </div>
-      <p class="p-0">
-        <span @click="land.btnCh=2">계정이 필요하신가요?</span>
-      </p>
     </div>
-    <button class="login-btn">로그인</button>
-  </form>
 </template>
 
 <script>
@@ -54,14 +69,11 @@ export default {
 </script>
 
 <style>
-.p-0 {
-  padding: 0px;
-}
 .flex {
   display: flex;
 }
-.justify-space-between {
-  justify-content: space-between;
+.justify-center {
+  justify-content: center;
 }
 .align-center {
   align-items: center;
@@ -74,25 +86,33 @@ export default {
   cursor:pointer;
 }
 .signin-head {
-  margin: 40px auto;
+  margin: 32px auto;
 }
 .signin-input {
   text-align: left;
   margin-top: 12px;
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+  border-bottom: 1px solid #c1bbbb;
 }
-.login-text {
-  
+.signin-input:focus-within {
+  border-bottom: 2px solid #c1bbbb;
+}
+.icon-signin {
+  color: #C699FF;
+  font-size: 20px;
+  margin-left: 12px;
 }
 .find-id-password {
   text-align: right;
-  margin-bottom: 16px;
 }
 .login-btn {
   font-weight: 600;
   font-size: 16px;
   width: 412px;
   height: 48px;
-  margin-top: 8px;
+  margin-top: 24px;
 }
 #signInForm div label {
   width: 44px;
@@ -102,14 +122,15 @@ export default {
   line-height: 20px;
   color: #000000;
 }
-#signInForm div input, select {
+#signInForm div input {
   box-sizing: border-box;
-  width: 412px;
+  width: 370px;
   height: 40px;
-  background: #FFFFFF;
-  border: 1px solid #C1BBBB;
-  border-radius: 8px;
-  padding: 10px;
+  border: 0;
+  margin-left: 16px;
+}
+#signInForm div input:focus {
+  outline: none;
 }
 #signInForm div {
   padding-top: 4px;
@@ -122,5 +143,32 @@ export default {
   cursor: pointer;
   color:#8227fa;
   text-decoration: underline;
+}
+.login-signup {
+  font-size: 16px;
+  color: #C699FF;
+  margin-bottom: 32px;
+}
+.login-signup span:hover {
+  cursor: pointer;
+  color:#8227fa;
+  text-decoration: underline;
+}
+.hr-sect {
+  display: flex;
+  flex-basis: 100%;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.35);
+  font-size: 12px;
+  margin: 16px 0px;
+}
+.hr-sect::before, .hr-sect::after {
+  content: "";
+  flex-grow: 1;
+  background: rgba(0, 0, 0, 0.35);
+  height: 1px;
+  font-size: 0px;
+  line-height: 0px;
+  margin: 0px 16px;
 }
 </style>
