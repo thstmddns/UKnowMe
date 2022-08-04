df00b18c8741e1f731e83175247a6210f3a83834
이동민 <jewellove1023@gmail.com>
2022-08-04 18:02:28 +0900

    [S07P12D104-63] feat: 비밀번호 찾기 화면구성 및 동작

--- FE/u-know-me/src/api/spring-rest.js
+++ FE/u-know-me/src/api/spring-rest.js
@@ -1,4 +1,4 @@
-const HOST = 'http://uknowme.mooo.com:8000/'
+const HOST = 'http://uknowme.mooo.com:8080/'
 
 const MEMBERS = 'member/'
 const MACHINGS = 'maching/'
@@ -9,43 +9,43 @@ const NOTICES = 'notice/'
 
 export default {
   members: {
-    login: () => HOST + MEMBERS + 'login/',
-    logout: () => HOST + MEMBERS + 'logout/',
-    signup: () => HOST + MEMBERS + 'join/',
-    findId: () => HOST + MEMBERS + 'find/id/',
-    findPassword: () => HOST + MEMBERS + 'find/password/',
-    account: memberSeq => HOST + MEMBERS + `${memberSeq}/`,
-    idDuplicate: () => HOST + MEMBERS + 'check/id/',
-    nickNameDuplicate: () => HOST + MEMBERS + 'check/nickname/',
-    kakaoLogin: () => HOST + MEMBERS + 'kakao/callback/',
-    naverLogin: () => HOST + MEMBERS + 'naver/callback/',
-    kakao: () => HOST + MEMBERS + 'add/kakao/',
-    naver: () => HOST + MEMBERS + 'add/naver/',
-    block: () => HOST + MEMBERS + 'block/',
-    report: () => HOST + MEMBERS + 'report/',
-    reportInfo: reportSeq => HOST + MEMBERS + 'report/' + `${reportSeq}/`,
+    login: () => HOST + MEMBERS + 'login',
+    logout: () => HOST + MEMBERS + 'logout',
+    signup: () => HOST + MEMBERS + 'join',
+    findId: () => HOST + MEMBERS + 'find/id',
+    findPassword: () => HOST + MEMBERS + 'find/password',
+    account: memberSeq => HOST + MEMBERS + `${memberSeq}`,
+    idDuplicate: () => HOST + MEMBERS + 'check/id',
+    nickNameDuplicate: () => HOST + MEMBERS + 'check/nickname',
+    kakaoLogin: () => HOST + MEMBERS + 'kakao/callback',
+    naverLogin: () => HOST + MEMBERS + 'naver/callback',
+    kakao: () => HOST + MEMBERS + 'add/kakao',
+    naver: () => HOST + MEMBERS + 'add/naver',
+    block: () => HOST + MEMBERS + 'block',
+    report: () => HOST + MEMBERS + 'report',
+    reportInfo: reportSeq => HOST + MEMBERS + 'report/' + `${reportSeq}`,
   },
   machings: {
-    oneToOne: memberSeq => HOST + MACHINGS + '1vs1/' + `${memberSeq}/`,
-    twoToTwo: memberSeq => HOST + MACHINGS + '2vs2/' + `${memberSeq}/`,
+    oneToOne: memberSeq => HOST + MACHINGS + '1vs1/' + `${memberSeq}`,
+    twoToTwo: memberSeq => HOST + MACHINGS + '2vs2/' + `${memberSeq}`,
   },
   features: {
-    balance: () => HOST + FEATURES + 'balance/',
-    balanceInfo: balanceSeq => HOST + FEATURES + 'balance/' + `${balanceSeq}/`,
-    keyword: () => HOST + FEATURES + 'keyword/',
-    keywordInfo: keywordSeq => HOST + FEATURES + 'keyword/' + `${keywordSeq}/`,
+    balance: () => HOST + FEATURES + 'balance',
+    balanceInfo: balanceSeq => HOST + FEATURES + 'balance/' + `${balanceSeq}`,
+    keyword: () => HOST + FEATURES + 'keyword',
+    keywordInfo: keywordSeq => HOST + FEATURES + 'keyword/' + `${keywordSeq}`,
   },
   rooms: {
-    like: memberSeq => HOST + ROOMS + 'like/' + `${memberSeq}/`,
-    exit: () => HOST + ROOMS + 'exit/',
-    entrance: () => HOST + ROOMS + 'entrance/',
+    like: memberSeq => HOST + ROOMS + 'like/' + `${memberSeq}`,
+    exit: () => HOST + ROOMS + 'exit',
+    entrance: () => HOST + ROOMS + 'entrance',
   },
   avatars: {
-    avatar: () => HOST + AVATARS,
-    avatarInfo: avatarSeq => HOST + AVATARS + `${avatarSeq}/`,
+    avatar: () => HOST + 'avatar',
+    avatarInfo: avatarSeq => HOST + AVATARS + `${avatarSeq}`,
   },
   notices: {
-    notice: () => HOST + NOTICES,
-    noticeInfo: noticeSeq => HOST + NOTICES + `${noticeSeq}/`,
+    notice: () => HOST + 'notice',
+    noticeInfo: noticeSeq => HOST + NOTICES + `${noticeSeq}`,
   }
 }
---
+++ FE/u-know-me/src/components/land/modal/FindPassword.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="sign-head signin-head">
+    <i class="fa-solid fa-lock-open"></i>&#160;비밀번호 찾기
+  </div>
+  <form id="findPasswordForm" action="POST" @submit.prevent="account.findPassword(credentials)">
+    <div>
+      <div><label for="findPasswordId">아이디</label></div>
+      <div><input type="text" name="findPasswordId" id="findPasswordId" placeholder="아이디를 입력해주세요." v-model="credentials.name"/></div>
+    </div>
+    <div>
+      <div><label for="findPasswordName">이름</label></div>
+      <div><input type="text" name="findPasswordName" id="findPasswordName" placeholder="이름을 입력해주세요." v-model="credentials.name"/></div>
+    </div>
+    <div>
+      <div><label for="findPasswordTel">휴대폰 번호</label></div>
+      <div>
+        <input class="middle-input" type="password" name="findPasswordTel" id="findPasswordTel" placeholder="-없이 입력해주세요." v-model="credentials.tel"/>
+        <button type="button">전송하기</button>
+      </div>
+    </div>
+    <div class="mb-16px">
+      <div><label for="findPasswordCertificationNumber">인증번호</label></div>
+      <div>
+          <input class="middle-input" type="text" name="findPasswordCertificationNumber" id="findPasswordCertificationNumber"  placeholder="인증번호를 입력해주세요.">
+          <button type="button">인증하기</button>
+        </div>
+    </div>
+    <button type="submit" class="sign-btn">비밀번호 찾기</button>
+  </form>
+  <div class="go-login">
+    <span @click="land.btnCh=7">비번 찾았으</span> | 
+    <span @click="land.btnCh=1">로그인 창으로 돌아가기</span>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { useAccountStore } from '@/stores/land/account'
+import { useLandStore } from '@/stores/land/land'
+
+export default {
+  name:'FindPassword',
+  setup() {
+    const account = useAccountStore()
+    const land = useLandStore()
+    const credentials = ref({
+      id: '',
+      name: '',
+      tel: '',
+    })
+    return {
+      account,
+      credentials,
+      land,
+    }
+  }
+}
+</script>
+
+<style>
+#findPasswordForm {
+  height: calc(100% - 120px);
+  margin-right: -32px;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+#findPasswordForm::-webkit-scrollbar {
+  width: 10px;
+}
+#findPasswordForm::-webkit-scrollbar-thumb {
+  height: 30%;
+  background: #A056FF;
+  border-radius: 10px;
+}
+#findPasswordForm::-webkit-scrollbar-track {
+  background: rgba(160, 86, 255, .1);
+}
+#findPasswordForm div {
+  padding-top: 4px;
+}
+#findPasswordForm div label {
+  width: 44px;
+  height: 20px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  color: #000000;
+}
+#findPasswordForm div input, select {
+  box-sizing: border-box;
+  width: 412px;
+  height: 40px;
+  background: #FFFFFF;
+  border: 1px solid #C1BBBB;
+  border-radius: 8px;
+  padding: 10px;
+}
+#findPasswordForm div .middle-input {
+  width: 296px;
+}
+#findPasswordForm div .sort-input {
+  width: 132px;
+  margin-right: 8px;
+}
+#findPasswordForm button {
+  box-sizing: border-box;
+  height: 40px;
+  background: #A056FF;
+  border: 1px solid #C1BBBB;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 20px;
+  text-align: center;
+  color: #FFFFFF;
+  margin-left: 8px;
+}
+#findPasswordForm div button {
+  width: 104px;
+}
+#findPasswordForm .sign-btn {
+    width: 412px;
+    margin: 32px 0px 4px 0px;
+}
+#findPasswordForm button:hover {
+  background: #8d39fc;
+}
+#findPasswordForm button:active {
+  background: #8122fe;
+}
+.mb-16px {
+  margin-bottom: 16px;
+}
+</style>
