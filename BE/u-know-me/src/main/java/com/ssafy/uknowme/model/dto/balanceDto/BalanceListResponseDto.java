e15ecdf5491d9403fd5db1a049db0a4ce869cefb
손승운 <thstmddns@naver.com>
2022-08-11 13:37:08 +0900

    [S07P12D104-127] refactor: BalanceLIstResponseDto, KeywordLsitResponseDto 부분에 seq추가 했습니다

--- BE/u-know-me/src/main/java/com/ssafy/uknowme/model/dto/balanceDto/BalanceListResponseDto.java
+++ BE/u-know-me/src/main/java/com/ssafy/uknowme/model/dto/balanceDto/BalanceListResponseDto.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 @Getter
 public class BalanceListResponseDto {
 
+    private int seq;
     private String question;
     private String answer1;
     private String answer2;
 
     public BalanceListResponseDto(Balance entity) {
+        this.seq = entity.getSeq();
         this.question = entity.getQuestion();
         this.answer1 = entity.getAnswer1();
         this.answer2 = entity.getAnswer2();
