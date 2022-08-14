2071fddf744893a690c60940dd4188cf9f7bfa27
신예솔 <eer0016@naver.com>
2022-08-14 11:15:59 +0900

    [S07P12D104-90] feat : 유효정보로 신고 , 신고후alert 미구현

--- FE/u-know-me/src/components/chat/AccuseModal.vue
+++ FE/u-know-me/src/components/chat/AccuseModal.vue
@@ -7,18 +7,17 @@
       <div class="accuse-modal-container">
         <div class="accuse-modal-title">신고하기</div>
         <div class="accuse-modal-content">
-          상대의 비매너행위* 시 신고를 하실 수 있습니다. <br>
-          허위 신고로 판단될 경우 적절한 조치가 있을 예정 <br>
-          신고하시겠습니까? 
+          상대의 <span class="bad-manners">비매너행위*</span> 시 신고를 하실 수 있습니다. <br>
+          허위 신고로 판단될 경우 적절히 조치됩니다. <br>
+          신고하시겠습니까?
         </div>
         <div class="accuse-modal-content-sub">(비매너행위* : 욕설, 혐오 발언, 부적절한 행동)</div>
-        <button v-for="(people, i ) in otherPeople"
-        :people="people"
-        :key="i" 
-       >{{people.userName}}</button>
-        
-        <div class="accuse-btn-list">
-          <button class="accuse-btn"  v-on:click="createReport()">신고</button>
+
+
+        <div class="accuse-btn-list" v-for="(people, i ) in otherPeople" :people="people" :key="i">
+          <button v-on:click="createReport(account.currentUser.seq, people.userSeq), accuseBtn = 0"
+            class="accuse-btn">{{ people.userName }}</button>
+
         </div>
       </div>
     </div>
@@ -48,31 +47,31 @@ export default {
       otherPeople,
     };
   },
-  methods : {
-     createReport () {
+  methods: {
+    createReport(accusedMemberSeq, reportingMemberSeq) {
       axios({
         url: `https://uknowme.mooo.com:8443/report/create`,
         method: 'post',
-        data : {
-
-
-  "accusedMemberSeq": 111,
-  "reportState": "REPORT",
-  "reportingMemberSeq": 112
-
-
-
+        data: {
+          "accusedMemberSeq": accusedMemberSeq,
+          "reportState": "REPORT",
+          "reportingMemberSeq": reportingMemberSeq
         }
       })
         .then(res => {
           console.log(res);
+          const modalOff = document.querySelector(".close-btn");
+          console.log(modalOff)
+
+          modalOff.accuseBtn = 0;
+
         })
         .catch(err => {
           console.error(err.response)
         })
     }
-    }
   }
+}
 
 </script>
 
@@ -90,7 +89,7 @@ export default {
 .accuse-modal {
   position: relative;
   width: 440px;
-  height: 350px;
+  min-height: 350px;
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 27px;
@@ -98,10 +97,12 @@ export default {
   top: 50%;
   transform: translate(-50%, -50%);
 }
+
 .accuse-modal-container {
   padding: 32px 40px;
   /* display: inline; */
 }
+
 .accuse-modal-title {
   padding: 2% 11%;
   text-align: center;
@@ -109,26 +110,38 @@ export default {
   font-weight: 700;
   color: #8227fa;
 }
+
 .accuse-modal-content {
   margin: 6% 0;
   text-align: center;
   line-height: 30px;
 }
+
 .accuse-modal-content-sub {
   font-weight: 700;
   text-align: center;
+   color: rgb(153, 84, 199);
 }
+
 .accuse-btn-list {
   display: flex;
   justify-content: center;
   margin-top: 6%;
 }
+
 .accuse-btn {
+  width: 200px;
   font-size: 15px;
   font-weight: 700;
 }
+
 .accuse-btn:hover {
   background-color: rgb(255, 51, 0);
   border: none;
+}
+
+.bad-manners {
+  color: rgb(153, 84, 199);
+
 }
 </style>
